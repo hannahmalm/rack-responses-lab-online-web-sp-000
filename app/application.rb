9223ca93554b60d98583d8a0d15fc5a09e64a0b7
Hannah M <hannahemalm616@gmail.com>
2021-04-25 00:29:04 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,17 @@
+#if its before noon, greet the user with "Good Morning!"
+#if its after noon, greet the user with "Good Afternoon! Getting the time may return 24 hour time. Anytime on or after 12 is the afternoon"
+
+class Application
+def call(env)
+    resp = Rack::Response.new
+ 
+    t = Time.now 
+    if t.hour == 12 || 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9 || 10 || 11 
+      resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
+    end
+ 
+    resp.finish
+  end
+end
